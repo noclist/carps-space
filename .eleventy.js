4f5313fe13224078d96cfa224faedf2ac1e481e9
Ben Carpenter <carp131@msn.com>
2021-08-13 15:49:40 -0400

    11ty, njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy('./src/icons');
+    eleventyConfig.addPassthroughCopy('./src/images');
+    eleventyConfig.addPassthroughCopy('./src/js');
+
     return {
         dir: {
-            input: ""
+            input: "src",
+            output: "public"
         }
     };
 }
